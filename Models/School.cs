f7483704ea1813712c8a6533e719b9f1d655c1b3
juanm <juanm@DESKTOP-RCU89TN>
2022-09-22 09:05:30 -0300

    Agregar archivos de proyecto.

---
+++ Models/School.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CuentaRegresiva.Models
+{
+    public class School //ENTIDAD ESCUELA int, decimal, string
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
